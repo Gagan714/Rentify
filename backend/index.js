38cdad1f0e73b394f50304ce3bb4f6b46476ed65
Gagan714 <gaganursmg@gmail.com>
2024-05-25 22:41:45 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,10 @@ app.use(cors());
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
